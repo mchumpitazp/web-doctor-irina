3740b96cf3e690c330242c4dc1e32e96363f5390
Mauro Polino <mchumpitazp@gmail.com>
2025-01-12 23:49:22 +0300

    uppercase removed

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -13,8 +13,8 @@ export default function Example() {
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-5">
                     <div className="col-span-2">
-                        <h2 className="text-uppercase text-lg/8 font-semibold text-accent">
-                            ваш надежный врач
+                        <h2 className="text-lg/8 font-semibold text-accent">
+                            Ваш надежный врач
                         </h2>
                         <p className="mt-2 text-pretty font-header text-4xl font-semibold tracking-tight text-primary sm:text-5xl">
                             Обо мне
