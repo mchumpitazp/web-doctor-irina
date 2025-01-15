3f9d85ca5f9ee3aa0d0bc899c0515be4addb342a
Mauro Polino <mchumpitazp@gmail.com>
2025-01-15 18:45:19 +0300

    svg imports

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { JSX, SVGProps, useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import ButtonScrollable from "./ButtonScrollable";
