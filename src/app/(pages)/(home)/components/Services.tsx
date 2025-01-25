07d9d6091734a77815ffb7e97ac57586f84e46d9
Mauro Polino <mchumpitazp@gmail.com>
2025-01-25 23:34:26 +0300

    improvements

--- src/app/(pages)/(home)/components/Services.tsx
+++ src/app/(pages)/(home)/components/Services.tsx
@@ -12,8 +12,8 @@ export default function Services() {
                 </h2>
                 <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
                     <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white/60 lg:rounded-l-[2rem]"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-l-[calc(2rem+1px)]">
+                        <div className="absolute inset-px rounded-lg bg-white/60 max-lg:rounded-t-[2rem] lg:rounded-l-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)] lg:rounded-l-[calc(2rem+1px)]">
                             <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                                 <p className="mt-2 text-xl font-medium tracking-tight max-lg:text-center">
                                     Онлайн-консультация
@@ -64,11 +64,11 @@ export default function Services() {
                                 </div>
                             </div>
                         </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-l-[2rem]"></div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem] lg:rounded-l-[2rem]"></div>
                     </div>
-                    <div className="relative max-lg:row-start-1">
-                        <div className="absolute inset-px rounded-lg bg-white/60 max-lg:rounded-t-[2rem]"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-t-[calc(2rem+1px)]">
+                    <div className="relative max-lg:row-start-2">
+                        <div className="absolute inset-px rounded-lg bg-white/60"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
                             <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                                 <p className="mt-2 text-xl font-medium tracking-tight max-lg:text-center">
                                     Метод “Сухих игл”
@@ -98,11 +98,11 @@ export default function Services() {
                                 />
                             </div>
                         </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-t-[2rem]"></div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
                     </div>
-                    <div className="relative max-lg:row-start-3 lg:col-start-2 lg:row-start-2">
-                        <div className="absolute inset-px rounded-lg bg-white/60"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]">
+                    <div className="relative max-lg:row-start-4 lg:col-start-2 lg:row-start-2">
+                        <div className="absolute inset-px rounded-lg bg-white/60 max-lg:rounded-b-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)]">
                             <div className="px-8 pt-8 sm:px-10 sm:pt-10">
                                 <p className="mt-2 text-xl font-medium tracking-tight max-lg:text-center">
                                     Полезная информация
@@ -130,11 +130,11 @@ export default function Services() {
                                 />
                             </div>
                         </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5"></div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem]"></div>
                     </div>
-                    <div className="relative lg:row-span-2">
-                        <div className="absolute inset-px rounded-lg bg-white/60 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
-                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                    <div className="relative max-lg:row-start-3 lg:row-span-2">
+                        <div className="absolute inset-px rounded-lg bg-white/60 lg:rounded-r-[2rem]"></div>
+                        <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] lg:rounded-r-[calc(2rem+1px)]">
                             <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
                                 <p className="mt-2 text-xl font-medium tracking-tight max-lg:text-center">
                                     Кинезиотейпирование
@@ -166,7 +166,7 @@ export default function Services() {
                                 </div>
                             </div>
                         </div>
-                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]"></div>
+                        <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5 lg:rounded-r-[2rem]"></div>
                     </div>
                 </div>
             </div>
