67365ea2e71e1818dcbd56a4f1dfad7861471f90
Mauro Polino <mchumpitazp@gmail.com>
2025-02-01 19:05:47 +0300

    yandex metrica test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import { Raleway, Mulish } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Script from "next/script";
 
 // Configure Raleway
 const raleway = Raleway({
@@ -46,8 +48,37 @@ export default function RootLayout({
                     href="/favicon-16x16.png"
                 />
                 <link rel="manifest" href="/site.webmanifest" />
+                {/* Yandex.Metrika counter */}
+                <Script
+                    id="yandex-metrica"
+                    strategy="afterInteractive"
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                            m[i].l=1*new Date();
+                            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                            ym(99753768, "init", {
+                                clickmap:true,
+                                trackLinks:true,
+                                accurateTrackBounce:true
+                            });
+                            `,
+                    }}
+                />
             </head>
             <body>
+                <noscript>
+                    <div>
+                        <img
+                            src="https://mc.yandex.ru/watch/99753768"
+                            style={{ position: "absolute", left: "-9999px" }}
+                            alt=""
+                        />
+                    </div>
+                </noscript>
                 <Header />
                 {children}
                 <Footer />
